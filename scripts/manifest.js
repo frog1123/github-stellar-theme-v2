c0d6821d49b0cc2f8e7721107ec8d6ef76fdee51
frog1123 <93895449+frog1123@users.noreply.github.com>
2022-10-30 21:35:17 -0500

    Update manifest.js

--- scripts/manifest.js
+++ scripts/manifest.js
@@ -10,9 +10,14 @@ const contentScripts = [
   }
 ];
 
-const createManifest = async ({ contentScripts }) => {
+const createManifest = async ({ contentScripts, version }) => {
   try {
-    writeFileSync(join(__dirname, '..', 'dist', 'manifest.json'), `{"manifest_version":3,"version":"1.0.0","name":"github stellar theme","author":"frog1123","description":"stellar theme for github","permissions":["contextMenus","activeTab","scripting","storage"],"content_scripts":${JSON.stringify(contentScripts)},"icons":{"16":"16.png","32":"32.png","48":"48.png","64":"64.png","128":"128.png"},"action":{"default_icon":{"16":"16.png","32":"32.png","48":"48.png","64":"64.png","128":"128.png"},"default_title":"github stellar theme","default_popup":"popup.html"},"options_page": "options.html"}`);
+    writeFileSync(
+      join(__dirname, '..', 'dist', 'manifest.json'),
+      `{"manifest_version":3,"version":\"${version}\","name":"github stellar theme","author":"frog1123","description":"stellar theme for github","permissions":["contextMenus","activeTab","scripting","storage"],"content_scripts":${JSON.stringify(
+        contentScripts
+      )},"icons":{"16":"16.png","32":"32.png","48":"48.png","64":"64.png","128":"128.png"},"action":{"default_icon":{"16":"16.png","32":"32.png","48":"48.png","64":"64.png","128":"128.png"},"default_title":"github stellar theme","default_popup":"popup.html"},"options_page": "options.html"}`
+    );
     console.log(`${chalk.green('✔')} created mainfest.json`);
   } catch (err) {
     console.log(err);
@@ -20,7 +25,7 @@ const createManifest = async ({ contentScripts }) => {
   }
 };
 
-createManifest({ contentScripts });
+createManifest({ contentScripts, version: '1.0.1' });
 
 // {
 //   "manifest_version": 3,
